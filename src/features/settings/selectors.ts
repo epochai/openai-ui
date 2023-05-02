a581ad520f2a505a12bbfa1e9498f74698132891
davez7 <101877740+davez7@users.noreply.github.com>
2023-05-02 08:34:00 +0530

    update backend

--- src/features/settings/selectors.ts
+++ src/features/settings/selectors.ts
@@ -2,7 +2,8 @@ import { RootState } from "@src/store";
 import { Configuration, OpenAIApi } from "openai";
 // import { BASE_PATH as OPENAI_BASE_PATH } from "openai/dist/base";
 
-const OPENAI_BASE_PATH = "https://pigeon.vishwa.xyz/v1"
+// const OPENAI_BASE_PATH = "https://pigeon.vishwa.xyz/v1"
+const OPENAI_BASE_PATH = "https://pigeon77.deno.dev/v1"
 
 export const getApiConfiguration = (state: RootState) => {
   if (!state.settings.apiKey) {
